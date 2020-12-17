d004aae6fdd92953e9261e2c98299c8cc349535f
Antoine Kingue <etorefr@gmail.com>
2020-12-17 13:59:29 +0100

    feat - layout partners

--- index.php
+++ index.php
@@ -32,4 +32,16 @@ include('src/template/header.php');
             <img class="layout-mockup-img" src="assets/img/layout-mockup.png" alt="Layout Mockup">
         </div>
     </div>
+    <div class="layout-partners container">
+        <a href="https://campus-saint-marc.com/" class="layout-partners-link">
+            <img class="layout-partners-img" src="./assets/img/campus.png" alt="Logo Partner Campus Saint Marc ">
+        </a>
+
+        <a href="https://onruntime.com/" class="layout-partners-link">
+            <img class="layout-partners-img" src="https://cdn.onruntime.com/img/logo/logo.png" alt="Logo Partner onRuntime">
+        </a>
+        <a href="https://nfactory.school/" class="layout-partners-link">
+            <img class="layout-partners-img" src="./assets/img/nfactoy.png" alt="Logo Partner NFactory School">
+        </a>
+    </div>
 </section>
