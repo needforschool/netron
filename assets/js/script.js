0a1bd3018f6a9bf9c164306184988c8f68e18530
Antoine Kingue <etorefr@gmail.com>
2020-12-30 02:32:55 +0100

    style - modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,47 @@ new Swiper('.swiper-container', {
 });
 
 /* navbar responsive */
-header = document.getElementById('header');
+const header = document.getElementById('header');
+const navResponsive = header.querySelector('.nav-responsive')
 header.querySelector('.nav-burger').addEventListener('click', () => {
-  let responsiveVisible = header.querySelector('.nav-responsive').style.display;
-  if (responsiveVisible == 'none' || !responsiveVisible) responsiveVisible = 'flex';
-  header.querySelector('.nav-responsive').style.display = responsiveVisible;
-
-  header.querySelector('.nav-responsive').classList.toggle('active');
+  if (navResponsive.style.display == 'none' || !navResponsive.style.display) navResponsive.style.display = 'flex';
+  navResponsive.classList.toggle('active');
 })
 
+/* modal */
+const modalsWrapper = header.querySelector('.modal-wrapper');
+const modalCloseButton = modalsWrapper.querySelector('.modal-close');
+const btnRegister = modalsWrapper.querySelector('[role="btn-register"]');
+const btnLogin = modalsWrapper.querySelector('[role="btn-login"]');
+const formRegister = modalsWrapper.querySelector('.form-register');
+const formLogin = modalsWrapper.querySelector('.form-login');
+
+modalCloseButton.addEventListener('click', () => {
+  modalsWrapper.classList.toggle('active');
+})
+
+modalsWrapper.addEventListener('click', (e) => {
+  if (e.target == modalsWrapper) {
+    modalsWrapper.classList.toggle('active');
+  }
+})
+
+header.querySelectorAll('[role="btn-modal-login"]').forEach(element => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    navResponsive.classList.remove('active');
+    modalsWrapper.classList.toggle('active');
+  });
+})
+
+btnRegister.addEventListener('click', (e) => {
+  e.preventDefault();
+  formLogin.style.display = 'none';
+  formRegister.style.display = 'flex';
+})
+
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  formRegister.style.display = 'none';
+  formLogin.style.display = 'flex';
+})
